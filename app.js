99beffed95e6694d80ed7250458fe742ca30803c
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-02-08 12:06:04 +0000

    adding tiles and keys

---
+++ app.js
@@ -0,0 +1,33 @@
+const tileDisplay = document.querySelector('#tile-container');
+const tileDisplay = document.querySelector('#key-container');
+
+const key = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '«',
+]
