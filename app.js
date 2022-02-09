1d35ba307b9f8baf42c0c18ff119c15b233e2a45
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-02-09 00:15:27 +0000

    adding guessing functionality

--- app.js
+++ app.js
@@ -48,11 +48,16 @@ guessRows.forEach((guessRow, guessRowIndex) => {
     const rowElement = document.createElement('div')
     rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
 
+    guessRow.forEach((_guess, guessIndex) => {
+        const tileElement = document.createElement('div')
+        tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
+        tileElement.classList.add('tile')
+        rowElement.append(tileElement)
 
+    })
 
-})
+    tileDisplay.append(rowElement)
 
-tileDisplay.append(rowElement)
 })
 
 
